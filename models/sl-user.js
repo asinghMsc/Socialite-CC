10f25a54efb250e2b94d6c1259c2734d9b62dbe9
asinghMsc <asingh20@student.bbk.ac.uk>
2022-11-12 17:02:06 +0000

    user & post schema

--- models/sl-user.js
+++ models/sl-user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 
+//creating a schema for the user
 const sl_User_Schema = mongoose.Schema({
     username: {
         type: String,
@@ -13,5 +14,56 @@ const sl_User_Schema = mongoose.Schema({
         min:6,
         max:255
     },
+    password:{
+        type: String,
+        required: true,
+        min:6,
+        max:1024
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    }
 
 })
+
+// post schema
+const sl_Post_Schema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        min:4,
+        max:255
+    },
+    description: {
+        type: String,
+        required: true,
+        min:6,
+        max:1024
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'sl_User'
+    },
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'sl_Comment'
+    }],
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'sl_User'
+    }],
+    dislikes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'sl_User'
+    }]
+  
+    
+})
+
+//exporting the model
+module.exports = mongoose.model('sl_User', sl_User_Schema)
